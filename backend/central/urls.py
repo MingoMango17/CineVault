8c9337d5a1aaf512f9d68f9ec76e7ba977f89e74
MingoMango17 <junry.mingo17@gmail.com>
2025-07-26 00:47:39 +0800

    fix: change inherited class to ModelSerializer

--- backend/central/urls.py
+++ backend/central/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from movies.urls import router as movie_router
 
 urlpatterns = [
+    path('api/v1/', include(movie_router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
 ]
--- backend/movies/serializers.py
+++ backend/movies/serializers.py
@@ -1,13 +1,13 @@
 from rest_framework import serializers
 from .models import Movie
 
-class MovieDetailSerializer(serializers.Serializer):
+class MovieDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = ["id", "title", "description", "date_added", "video_file"]
         read_only_fields = ["date_added"]
 
-class MovieListSerializer(serializers.ListSerializer):
+class MovieListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = ["id", "title", "date_added"]
