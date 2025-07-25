663978aea27f60f929022dbc9553b3bcc3a0e151
MingoMango17 <junry.mingo17@gmail.com>
2025-07-26 00:36:02 +0800

    feat: add movie vew

--- backend/movies/views.py
+++ backend/movies/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Movie
+from .serializers import MovieListSerializer, MovieDetailSerializer
 
-# Create your views here.
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return MovieListSerializer
+        return MovieDetailSerializer
+
+    def get_permissions(self):
+        if self.action == 'list' or 'retrieve':
+            permission_classes = [permissions.AllowAny]
+        else:
+            permission_classes = [permissions.IsAuthenticated]
+        return [permission() for permission in permission_classes]
