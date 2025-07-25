773a472f1cb000e6511809bfd1362c0e05a5efe1
MingoMango17 <junry.mingo17@gmail.com>
2025-07-25 23:34:53 +0800

    feat: add movie model

--- backend/movies/models.py
+++ backend/movies/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.core.validators import FileExtensionValidator
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=255, null=False, blank=False)
+    description = models.TextField(null=False, blank=False)
+    date_added = models.DateField(auto_now_add=True)
+    video_file = models.FileField(
+        upload_to='videos/',
+        null=True,
+        validators=[FileExtensionValidator(\
+            allowed_extensions=['mp4', 'webm', 'mkv']
+        )]
+    )
+
