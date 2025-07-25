ac2e1e5badb7e0b18419d8484dcf6d9aa4fe1d12
MingoMango17 <junry.mingo17@gmail.com>
2025-07-25 23:54:31 +0800

    chore: add movie model admin

--- backend/movies/admin.py
+++ backend/movies/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Movie
 
 # Register your models here.
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_fields = ("title", "date_added")
+    readonly_fields = ("date_added",)
