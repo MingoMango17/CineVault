03448a9ff2a047a3d304a99ca0f34e763bdd0bc0
MingoMango17 <junry.mingo17@gmail.com>
2025-07-28 04:36:43 +0800

    chore: add migrations for added attributes for movie model

--- backend/movies/models.py
+++ backend/movies/models.py
@@ -1,14 +1,23 @@
 from django.db import models
 from django.core.validators import FileExtensionValidator
 
+
 class Movie(models.Model):
     title = models.CharField(max_length=255, null=False, blank=False)
     description = models.TextField(null=False, blank=False)
     date_added = models.DateField(auto_now_add=True)
     video_file = models.FileField(
-        upload_to='videos/',
+        upload_to="videos/",
         null=True,
-        validators=[FileExtensionValidator(\
-            allowed_extensions=['mp4', 'webm', 'mkv']
-        )]
+        validators=[FileExtensionValidator(allowed_extensions=["mp4", "webm", "mkv"])],
+    )
+
+    date_released = models.DateField(null=True, blank=True)
+    duration = models.IntegerField(null=True, blank=True)
+    director = models.CharField(max_length=255, null=True, blank=True)
+    poster_url = models.CharField(max_length=511, null=True, blank=True)
+    poster_image = models.FileField(
+        upload_to="images/",
+        null=True,
+        blank=True
     )
